c7dc657d4451c6d5b05f60ec7a30a02c0061bb6d
LuiSuarez21 <joselce1999@gmail.com>
2025-04-30 22:09:32 +0100

    Adding a option to see a meta of a 7 days~

--- funcAux.py
+++ funcAux.py
@@ -8,8 +8,8 @@ def add_new_decks_file_txt (fileName, array_decks):
     file.close()
 
 
-def fetch_decks(decks, regex_expression, regex_pattern_clean_name, array_decks):
-    for deck in decks:
+def fetch_decks(decks_html, regex_expression, regex_pattern_clean_name, array_decks):
+    for deck in decks_html:
         deckString = str(deck)
         result_search = re.search(regex_expression, deckString)
 
@@ -17,6 +17,13 @@ def fetch_decks(decks, regex_expression, regex_pattern_clean_name, array_decks):
             deck_name = result_search.group(1)
             clean_deck_name = re.sub(regex_pattern_clean_name, '', deck_name)
             array_decks.append(clean_deck_name)
+
+
+def fetch_option(options_html, regex_expression):
+    for option in options_html:
+        option_string = str(option)
+        result_search = re.search(regex_expression, option_string)
+        re.findall(r'[0-9]{2}', str(result_search))
 
 
 def menu(array_decks):
@@ -27,12 +34,35 @@ def menu(array_decks):
     print("4) Give me a deck list of a certain deck;")
     option = int(input("\nChoose an option: "))
 
-    while option in range(1, 4) != False:
+    while option in range(1, 5) == False:
         print("-----------------------------------------\n\n")
         print("Error!!! Please insert a correct value!!!")
         print("1) All decks of the current meta on Modern;")
-        print("2) Prices of each deck;")
-        print("3) Top 3 positions on the last competitions of this Modern Decks;")
-        print("4) Give me a deck list of a certain deck;")
+        print("2) All decks of the current meta on Modern on the last 7 Days;")
+        print("3) Prices of each deck;")
+        print("4) Top 3 positions on the last competitions of this Modern Decks;")
+        print("5) Give me a deck list of a certain deck;")
         option = int(input("\nChoose an option: "))
 
+    match option:
+        case 1:
+            show_all_decks(array_decks)
+
+        case 2:
+            breakpoint()
+
+        case 3:
+            breakpoint()
+
+        case 4:
+            breakpoint()
+
+        case 5:
+            breakpoint()
+
+
+def show_all_decks(array_decks):
+    print("\n----------------------------")
+    print("All decks on the current Modern Format:\n")
+    for element in array_decks:
+        print(element)
--- main.py
+++ main.py
@@ -6,16 +6,21 @@ import funcAux
 # Extract each <class> with name "deck-price-online"
 web_page = requests.get("https://www.mtggoldfish.com/metagame/modern/full#paper")
 soup = BeautifulSoup(web_page.text, "html.parser")
-decks = soup.find_all(class_="deck-price-online")
+decks_html = soup.find_all(class_="deck-price-online")
+option_html = soup.find_all(class_="metagame-re-sort")
 
 # Regex patterns
 regex_expression = r'/archetype/modern-(.*?)#online'  # Extract deck name
+regex_expression_option_selected = r'<option selected="selected".+'
 regex_pattern_clean_name = r'-[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$'  # Remove full UUID (random letters we don't want)
+# I need now to parse my option on 7 days
+# And I need, probably, to clean the excessive lines of code that will comi with this fecth
 
 # Variable Declarations
 array_decks = []
 fileName = "decks_name.txt"
 
-funcAux.fetch_decks(decks, regex_expression, regex_pattern_clean_name, array_decks)
+funcAux.fetch_decks(decks_html, regex_expression, regex_pattern_clean_name, array_decks)
+funcAux.fetch_option(option_html, regex_expression_option_selected)
 funcAux.add_new_decks_file_txt(fileName, array_decks)
 funcAux.menu(array_decks)
