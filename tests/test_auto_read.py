f72b73704deb2bcbbcf94fcfd0580e6e82dae13a
LuiSuarez21 <joselce1999@gmail.com>
2025-05-06 17:04:04 +0100

    Addind some tests examples

--- tests/test_auto_read.py
+++ tests/test_auto_read.py
@@ -1,7 +1,3 @@
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
-
 import pytest
 from src.auto_read.funcAux import add_new_decks_file_txt, fetch_decks, fetch_option, show_all_decks
 
