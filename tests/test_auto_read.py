56700e73422f5ac860cb5286e7ff97723c55f1e4
LuiSuarez21 <joselce1999@gmail.com>
2025-05-06 16:57:17 +0100

    Adding some changes

--- tests/test_auto_read.py
+++ tests/test_auto_read.py
@@ -1,5 +1,10 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
+
 import pytest
 from src.auto_read.funcAux import add_new_decks_file_txt, fetch_decks, fetch_option, show_all_decks
+
 
 # ------------------------------
 # Test for add_new_decks_file_txt
